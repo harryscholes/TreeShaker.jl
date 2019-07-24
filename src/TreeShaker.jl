241c97eb5f6a46a6797df97cc6ef8b8cd509b502
Arnav Sood <me@arnavsood.com>
2019-07-24 15:47:59 -0400

    cleaning

--- src/TreeShaker.jl
+++ src/TreeShaker.jl
@@ -1,10 +1,6 @@
 module TreeShaker
 
 # Deps
-using SnoopCompile # for snooping
-using Random # for snoop log names
-using Pkg # context/manifest info
-using Suppressor # for optionally suppressing build & test output
 
 # Includes 
 include("trunk.jl")
